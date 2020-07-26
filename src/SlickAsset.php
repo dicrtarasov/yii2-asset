31a9e9524a557bba8ad24d672310c8986f9511b8
Igor A Tarasov <develop@dicr.org>
2020-07-26 11:26:00 +0500

    slick 1.8

--- src/SlickAsset.php
+++ src/SlickAsset.php
@@ -3,7 +3,7 @@
  * @copyright 2019-2020 Dicr http://dicr.org
  * @author Igor A Tarasov <develop@dicr.org>
  * @license proprietary
- * @version 26.07.20 10:41:29
+ * @version 26.07.20 11:25:43
  */
 
 declare(strict_types = 1);
@@ -20,19 +20,19 @@ use yii\web\JqueryAsset;
 class SlickAsset extends AssetBundle
 {
     /** @var string */
-    public const CSS_THEME = 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css';
+    public const CSS_THEME = 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css';
 
     /** @var bool включать тему */
     public $withTheme = false;
 
     /** @inheritDoc */
     public $css = [
-        'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css',
     ];
 
     /** @inheritDoc */
     public $js = [
-        'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js'
+        'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js'
     ];
 
     /** @inheritDoc */
@@ -43,7 +43,7 @@ class SlickAsset extends AssetBundle
     /**
      * @inheritDoc
      */
-    public function init() : void
+    public function init()
     {
         if ($this->withTheme) {
             $this->css[] = self::CSS_THEME;
