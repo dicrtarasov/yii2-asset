c7376d926af0371e6282b6e5a7d3179356653751
Igor A Tarasov <develop@dicr.org>
2021-05-17 02:09:41 +0500

    scss: move compiled to assets

--- src/ScssConverter.php
+++ src/ScssConverter.php
@@ -3,22 +3,23 @@
  * @copyright 2019-2021 Dicr http://dicr.org
  * @author Igor A Tarasov <develop@dicr.org>
  * @license proprietary
- * @version 08.03.21 17:03:35
+ * @version 17.05.21 02:09:22
  */
 
 declare(strict_types = 1);
 namespace dicr\asset;
 
 use ScssPhp\ScssPhp\Compiler;
+use ScssPhp\ScssPhp\Exception\SassException;
 use ScssPhp\ScssPhp\OutputStyle;
 use Throwable;
 use Yii;
 use yii\base\Component;
 use yii\base\Exception;
+use yii\helpers\FileHelper;
 use yii\web\AssetConverterInterface;
 
 use function array_unique;
-use function basename;
 use function dirname;
 use function file_get_contents;
 use function file_put_contents;
@@ -28,6 +29,7 @@ use function strrpos;
 use function strtolower;
 use function substr;
 
+use const DIRECTORY_SEPARATOR;
 use const LOCK_EX;
 use const YII_ENV_DEV;
 
@@ -63,7 +65,7 @@ class ScssConverter extends Component implements AssetConverterInterface
     /**
      * @inheritDoc
      */
-    public function init() : void
+    public function init(): void
     {
         parent::init();
 
@@ -91,10 +93,10 @@ class ScssConverter extends Component implements AssetConverterInterface
      * @inheritDoc
      * @throws Exception
      */
-    public function convert($asset, $basePath) : string
+    public function convert($asset, $basePath): string
     {
         try {
-            return $this->compile($basePath, $asset);
+            return $this->convertAsset($basePath, $asset);
         } catch (Throwable $ex) {
             if (YII_ENV_DEV) {
                 throw new Exception('Ошибка компиляции: ' . $asset, 0, $ex);
@@ -107,17 +109,109 @@ class ScssConverter extends Component implements AssetConverterInterface
     }
 
     /**
+     * Компилирует файл.
+     *
+     * @param string $basePath
+     * @param string $asset
+     * @return string относительный путь файла результата или исходный файл
+     * @throws Exception
+     * @noinspection PhpUsageOfSilenceOperatorInspection
+     */
+    protected function convertAsset(string $basePath, string $asset): string
+    {
+        // получаем название файла результата
+        $cssName = $this->cssName($asset);
+        if ($cssName === null) {
+            return $asset;
+        }
+
+        // абсолютный путь исходного файла
+        $scssPath = $basePath . '/' . $asset;
+
+        // если исходный файл не существует то ошибка
+        if (! is_file($scssPath)) {
+            throw new Exception('Исходный файл не найден: ' . $scssPath);
+        }
+
+        // абсолютный путь результата
+        $cssPath = $this->resultPath($cssName);
+        $cssUrl = $this->resultUrl($cssName);
+
+        $cssDir = dirname($cssPath);
+        if (! FileHelper::createDirectory($cssDir, 0755)) {
+            throw new Exception('Ошибка создания директории: ' . $cssDir);
+        }
+
+        // если нужно перекомпилировать
+        if ($this->needRecompile($scssPath, $cssPath)) {
+            // читаем файл
+            $scss = @file_get_contents($scssPath);
+            if ($scss === false) {
+                throw new Exception('Ошибка чтения ресурса: ' . $scssPath);
+            }
+
+            $compiler = $this->createCompiler();
+
+            // устанавливаем базовый путь импорта файлов
+            $compiler->setImportPaths(array_unique([
+                dirname($scssPath)
+            ]));
+
+            // source map
+            $mapName = $cssName . '.map';
+            $mapPath = $this->resultPath($mapName);
+            $mapUrl = $this->resultUrl($mapName);
+
+            $compiler->setSourceMapOptions([
+                // absolute path to write .map file
+                'sourceMapWriteTo' => $mapPath,
+
+                // relative or full url to the above .map file
+                'sourceMapURL' => $mapUrl,
+
+                // (optional) relative or full url to the .css file
+                'sourceMapFilename' => $cssUrl,
+
+                // базовый путь исходного файла
+                'sourceMapBasepath' => dirname($scssPath),
+
+                // (optional) prepended to 'source' field entries for relocating source files
+                'sourceRoot' => '/'
+            ]);
+
+            // компилируем
+            try {
+                $result = $compiler->compileString($scss, $scssPath);
+            } catch (SassException | Throwable $ex) {
+                throw new Exception('Ошибка компиляции ресурса: ' . $scssPath . ': ' . $ex);
+            }
+
+            // сохраняем css-файл
+            if (@file_put_contents($cssPath, $result->getCss(), LOCK_EX) === false) {
+                throw new Exception('Ошибка записи ресурса: ' . $cssPath);
+            }
+
+            // сохраняем map-файл
+            if ($this->sourceMap && @file_put_contents($mapPath, $result->getSourceMap(), LOCK_EX) === false) {
+                throw new Exception('Ошибка записи ресурса: ' . $mapPath);
+            }
+
+            Yii::debug('SCSS скомпилирован в : ' . $cssPath, __METHOD__);
+        }
+
+        return $cssName;
+    }
+
+    /**
      * Создает компилятор.
      * Так как он не очищается после компиляции каждого файла, то требуется пересоздание.
      *
      * @return Compiler
      */
-    protected function createCompiler() : Compiler
+    protected function createCompiler(): Compiler
     {
         // создаем и инициализируем компилятор
         $compiler = new Compiler();
-
-        $compiler->setEncoding('utf-8');
 
         if (! empty($this->outputStyle)) {
             $compiler->setOutputStyle($this->outputStyle);
@@ -131,12 +225,12 @@ class ScssConverter extends Component implements AssetConverterInterface
     }
 
     /**
-     * Возвращает путь файла результата.
+     * Возвращает имя файла результата (меняет scss на css).
      *
      * @param string $asset относительный путь источника
      * @return ?string относительный путь результата или null, если конвертация не требуется
      */
-    protected function getResultPath(string $asset) : ?string
+    protected function cssName(string $asset): ?string
     {
         // позиция расширения файла
         $pos = strrpos($asset, '.');
@@ -152,6 +246,32 @@ class ScssConverter extends Component implements AssetConverterInterface
     }
 
     /**
+     * Возвращает полный путь результата.
+     *
+     * @param string $path
+     * @return string
+     */
+    protected function resultPath(string $path): string
+    {
+        $assetManager = Yii::$app->assetManager;
+
+        return Yii::getAlias($assetManager->basePath . DIRECTORY_SEPARATOR . ltrim($path, DIRECTORY_SEPARATOR));
+    }
+
+    /**
+     * Возвращает полный URL результата.
+     *
+     * @param string $path относительный путь css
+     * @return string
+     */
+    protected function resultUrl(string $path): string
+    {
+        $assetManager = Yii::$app->assetManager;
+
+        return Yii::getAlias($assetManager->baseUrl . '/' . ltrim($path, '/'));
+    }
+
+    /**
      * Определяет нужна ли перекомпиляция файла.
      *
      * @param string $src
@@ -159,7 +279,7 @@ class ScssConverter extends Component implements AssetConverterInterface
      * @return bool
      * @noinspection PhpUsageOfSilenceOperatorInspection
      */
-    protected function needRecompile(string $src, string $dst) : bool
+    protected function needRecompile(string $src, string $dst): bool
     {
         if ($this->force || ! @is_file($dst)) {
             return true;
@@ -177,82 +297,5 @@ class ScssConverter extends Component implements AssetConverterInterface
         }
 
         return false;
-    }
-
-    /**
-     * Компилирует файл.
-     *
-     * @param string $basePath
-     * @param string $asset
-     * @return string относительный путь файла результата или исходный файл
-     * @throws Exception
-     * @noinspection PhpUsageOfSilenceOperatorInspection
-     */
-    protected function compile(string $basePath, string $asset) : string
-    {
-        // получаем адрес назначения
-        $result = $this->getResultPath($asset);
-        if (empty($result)) {
-            return $asset;
-        }
-
-        // абсолютный путь исходного файла и результата
-        $src = $basePath . '/' . $asset;
-        $dst = $basePath . '/' . $result;
-
-        // если исходный файл не существует то ошибка
-        if (! is_file($src)) {
-            throw new Exception('Исходный файл не найден: ' . $src);
-        }
-
-        // если не нужно перекомпилировать то пропускаем
-        if (! $this->needRecompile($src, $dst)) {
-            return $result;
-        }
-
-        // читаем файл
-        $scss = @file_get_contents($src);
-        if ($scss === false) {
-            throw new Exception('Ошибка чтения ресурса: ' . $src);
-        }
-
-        $compiler = $this->createCompiler();
-
-        // устанавливаем базовый путь импорта файлов
-        $compiler->setImportPaths(array_unique([
-            dirname($src)
-        ]));
-
-        // sourceMap
-        if ($this->sourceMap) {
-            $baseName = basename($result);
-
-            $compiler->setSourceMapOptions([
-                // путь map-файла
-                'sourceMapWriteTo' => $basePath . '/' . $result . '.map',
-                // относительный url файла
-                'sourceMapURL' => $baseName . '.map',
-                // базовый путь исходного файла
-                'sourceMapBasepath' => dirname($src),
-                // относительный путь файла назначения
-                'sourceMapFilename' => $baseName
-            ]);
-        }
-
-        // компилируем
-        try {
-            $css = $compiler->compile($scss, $src);
-        } catch (Throwable $ex) {
-            throw new Exception('Ошибка компиляции ресурса: ' . $src . ': ' . $ex);
-        }
-
-        // сохраняем файл
-        if (@file_put_contents($dst, $css, LOCK_EX) === false) {
-            throw new Exception('Ошибка записи ресурса: ' . $dst);
-        }
-
-        Yii::debug('SCSS скомпилирован в : ' . $basePath . '/' . $result, __METHOD__);
-
-        return $result;
     }
 }
